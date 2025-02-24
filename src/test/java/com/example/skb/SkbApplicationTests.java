30ce605015ebf42602fb3d93de9b786ce5c096c8
Evgeny <Evgeny.Slavkin@urfu.me>
2025-02-24 18:11:07 +0500

    1 дз

---
+++ src/test/java/com/example/skb/SkbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
